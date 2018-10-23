ba6fff548670970fdbf286653e9549bd039e62eb
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2018-10-22 21:12:08 -0400

    Meu primeiro commit

---
+++ olamundo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace olamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Seja Bem Vindo");
+            Console.Write("Curso de desenvolvimento mobile");
+            Console.Write("Professor Ramon Rodrigues");
+        }
+    }
+}
